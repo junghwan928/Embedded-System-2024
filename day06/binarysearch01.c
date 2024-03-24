f156b2cffd281cbd30d3f6a3a6e97173b5e13be8
Roy-Lee <128778304+junghwan928@users.noreply.github.com>
2024-03-24 16:25:13 +0900

    Update

--- day06/binarysearch01.c
+++ day06/binarysearch01.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
 
-void binary(int *ary, int size)
+void binary(int *ary, int size, int target)
 {
-    while(first <= last) {
+    int first = 0;
+    int last = size - 1;
+    int middle;
+
+    while (first <= last) {
         middle = (first + last) / 2;
-        if(target == ary[middle]) {
+        if (target == ary[middle]) {
             printf("%d\n", ary[middle]);
+            return; // 찾은 후에는 함수를 종료합니다.
         }
         else {
-            if(target > ary[middle]) first = middle + 1;
+            if (target > ary[middle]) first = middle + 1;
             else last = middle - 1;
         }
     }
+    printf("찾는 원소가 없습니다.\n");
 }
 
-int main()
+void main()
 {
     int ary[] = {2, 4, 6, 8, 10, 12}; // 이진탐색은 정렬이 되어있어야 함
     int size = sizeof(ary) / sizeof(int);
-    int first = 0;
-    int last = 0;
-    int middle = 0;
+    int target = 2;
 
-    /*
-    while(first <= last) {
-        middle = (first + last) / 2;
-        if(target == ary[middle]) {
-            printf("%d\n", ary[middle]);
-        }
-        else {
-            if(target > ary[middle]) first = middle + 1;
-            else last = middle - 1;
-        }
-    }
-    */
+    binary(ary, size, target);
 }
